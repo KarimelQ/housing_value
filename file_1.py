1159a304dae54a45e78739b7ee09639ea6f3fc05
Karim El Qouns <karim.elqouns@7sensingsoftware.com>
2023-02-08 09:38:25 +0100

    Merge branch 'new_feature' of github.com:KarimelQ/housing_value into new_feature

--- file_1.py
+++ file_1.py
@@ -3,4 +3,15 @@ this is a new file.
 this is a new feature update.
 
 
+<<<<<<< HEAD
 new feature update.
+=======
+<<<<<<< HEAD
+new feature update.
+
+
+modification for amend
+=======
+new feature update.
+>>>>>>> 7d990bcd8039b2852c74433ad7b96c9e6ccb3ffb
+>>>>>>> 3f67070... Merge branch 'new_feature' of github.com:KarimelQ/housing_value into new_feature
